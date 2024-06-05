52f29e96a5173bddea685b8312c7043bd9b6e3ed
manuothatceguy <mothatceguy@itba.edu.ar>
2024-06-05 15:50:01 -0300

    feat: clean function in aux_functions.sh and adds .gitignore
    Co-Authored-By: snogueira1 <147627792+snogueira1@users.noreply.github.com>
    Co-Authored-By: santinopepe <146956051+santinopepe@users.noreply.github.com>
    Co-Authored-By: maralvarezz <146204185+maralvarezz@users.noreply.github.com>

--- aux_functions.sh
+++ aux_functions.sh
@@ -5,3 +5,8 @@ function remove_schema(){
     sed -i '/xmlns="http:\/\/www.sportradar.com\/nascar-ot3"/d' $1
     sed -i '/xmlns="http:\/\/www.sportradar.com\/nascar-ot3"/d' $2
 }
+
+function clean_prev(){
+    # Se eliminan los archivos descargados
+    rm drivers_list.xml drivers_standings.xml nascar_data.xml
+}
--- tpe.sh
+++ tpe.sh
@@ -5,28 +5,31 @@ source vars.sh
 # aux_functions.sh contiene las funciones auxiliares utilizadas (por ejemplo: remove_schema)
 source aux_functions.sh
 
-# Validar que $1 sea mayor o igual a 2013 y menor o igual a 2024
-while [[ $1 -lt 2013 || $1 -gt 2024 ]]; do
-    echo "¡Valor fuera de rango! Por favor ingrese un valor entre 2013 y 2024:"
-    read -r $1
-done
-
-# Para mejorar la claridad del código, se define la variable year para guardar el valor de $1
-year=$1
-
-# Validar que $2 sea "sc", "xf", "cw", "go" o "mc"
-while [[ $2 != "sc" && $2 != "xf" && $2 != "cw" && $2 != "go" && $2 != "mc" ]]; do
-    echo "¡Valor inválido! Por favor ingrese un valor entre 'sc', 'xf', 'cw', 'go' o 'mc':"
-    read -r $2
-done
-
-# Misma razón de la definición de variable type que en la variable year
-type=$2
-
 # Se define la función main que corre el programa, 
 # se usa esta función para evitar que el script corra si es que se descargó incompleto
 function main(){
     
+    # Se borran archivos de ejecuciones anteriores
+    clean_prev
+
+    # Validar que $1 sea mayor o igual a 2013 y menor o igual a 2024
+    while [[ $1 -lt 2013 || $1 -gt 2024 ]]; do
+        echo "¡Valor fuera de rango! Por favor ingrese un valor entre 2013 y 2024:"
+        read -r $1
+    done
+
+    # Para mejorar la claridad del código, se define la variable year para guardar el valor de $1
+    year=$1
+
+    # Validar que $2 sea "sc", "xf", "cw", "go" o "mc"
+    while [[ $2 != "sc" && $2 != "xf" && $2 != "cw" && $2 != "go" && $2 != "mc" ]]; do
+        echo "¡Valor inválido! Por favor ingrese un valor entre 'sc', 'xf', 'cw', 'go' o 'mc':"
+        read -r $2
+    done
+
+    # Misma razón de la definición de variable type que en la variable year
+    type=$2
+
     URL_Drivers="https://api.sportradar.com/nascar-ot3/${type}/${year}/drivers/list.xml?api_key=${SPORTRADAR_API}"
     URL_Standings="https://api.sportradar.com/nascar-ot3/${type}/${year}/standings/drivers.xml?api_key=${SPORTRADAR_API}"
     
@@ -41,8 +44,8 @@ function main(){
     remove_schema $drivers $standings
 
     # Se llama a los parsers para que hagan su trabajo, se asume que están correctamente instalados.
-    java net.sf.saxon.Query extract_nascar_data.xq 
-    echo "hola"
+    # java net.sf.saxon.Query extract_nascar_data.xq 
+    
     
 }
 
